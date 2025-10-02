d98c570369e1f9c138c2ef42d7980cc34168daa4
vireshgupta <vireshgupta844@gmail.com>
2025-10-02 13:05:02 +0530

    added

--- src/components/SortableTask.js
+++ src/components/SortableTask.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 import Task from "./Task";
+import { GripVertical } from "lucide-react"; // optional icon (you can replace with any)
 
 export default function SortableTask({ task, tasks, setTasks }) {
   const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: task.id });
@@ -12,8 +13,19 @@ export default function SortableTask({ task, tasks, setTasks }) {
   };
 
   return (
-    <li ref={setNodeRef} style={style} {...attributes} {...listeners}>
-      <Task task={task} tasks={tasks} setTasks={setTasks} />
+    <li ref={setNodeRef} style={style} className="flex items-start gap-2">
+      <span
+        {...attributes}
+        {...listeners}
+        className="cursor-grab p-2 text-slate-400 hover:text-slate-600"
+      >
+        <GripVertical size={18} />
+      </span>
+
+      {/* Task Content */}
+      <div className="flex-1">
+        <Task task={task} tasks={tasks} setTasks={setTasks} />
+      </div>
     </li>
   );
 }
